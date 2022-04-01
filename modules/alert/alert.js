2a378d9185b8a41ece3dca47b20db4b9604c2657
tobyck <84485533+tobyck@users.noreply.github.com>
2022-04-02 12:56:49 +1300

    Fixed regex in alert module

--- modules/alert/alert.js
+++ modules/alert/alert.js
@@ -1,22 +1,21 @@
+// add stylesheet
+var stylesheet = document.createElement("link");
+stylesheet.rel = "stylesheet";
+stylesheet.href = "https://tobyck.surge.sh/modules/alert/alert.css";
+document.head.appendChild(stylesheet);
+
 function alert(message, width = "450px", height = "") {
     // remove previous alert before creating a new one
-    if (document.getElementById("custom-alert-stylesheet")) {
-        ["stylesheet", "darken", "box"].map(el => `custom-alert-${el}`).forEach(el => document.getElementById(el).remove());
+    if (document.getElementById("custom-alert-darken")) {
+        ["darken", "box"].map(el => `custom-alert-${el}`).forEach(el => document.getElementById(el).remove());
     }
 
     // allow basic markdown inside the alert
     message = message.replace(/</g, "&lt;").replace(/>/g, "&gt;")
-                     .replace(/\[(.*)\]\(([^\s]+)\)/g, "<a href='$2' class='custom-alert-link'>$1</a>")
-                     .replace(/\*\*(.*)\*\*/g, "<b>$1</b>")
-                     .replace(/\*(.*)\*/g, "<i>$1</i>")
+                     .replace(/\[(.+)\]\(([^\s]+)\)/g, "<a href='$2' class='custom-alert-link'>$1</a>")
+                     .replace(/\*\*(.+?)\*\*/g, "<b>$1</b>")
+                     .replace(/\*(.+?)\*/g, "<i>$1</i>")
                      .replace(/\n/g, "<br>");
-
-    // add stylesheet
-    var stylesheet = document.createElement("link");
-    stylesheet.id = "custom-alert-stylesheet"
-    stylesheet.rel = "stylesheet";
-    stylesheet.href = "https://tobyck.surge.sh/modules/alert/alert.css";
-    document.head.appendChild(stylesheet);
 
     // create an element to darken the rest of the screen
     var darken = document.createElement("div");
@@ -37,9 +36,9 @@ function alert(message, width = "450px", height = "") {
     button.innerText = "OK";
     button.id = "custom-alert-button";
     button.onclick = () => {
-        stylesheet.remove();
         darken.remove();
         box.remove();
+        document.body.style.overflow = "auto";
     }
 
     // add elements to the box and page
@@ -47,4 +46,7 @@ function alert(message, width = "450px", height = "") {
     box.appendChild(button);
     document.body.appendChild(darken)
     document.body.appendChild(box);
+
+    // block scrolling
+    document.body.style.overflow = "hidden";
 }
